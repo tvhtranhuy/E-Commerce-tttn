d6c32733a97fc0ddfb0447e1424ce593c26e151f
tvhtranhuy <tvhtranhuy1408@gmail.com>
2024-09-29 13:51:24 +0700

    2hat

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); // Erase if already required
-// const bcrypt = require('bcrypt')
-// const crypto = require('crypto')
+const bcrypt = require('bcrypt')
+//const crypto = require('crypto')
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
     firstname: {
@@ -61,13 +61,13 @@ var userSchema = new mongoose.Schema({
     timestamps: true
 });
 
-// userSchema.pre('save', async function (next) {
-//     if (!this.isModified('password')) {
-//         next()
-//     }
-//     const salt = bcrypt.genSaltSync(10)
-//     this.password = await bcrypt.hash(this.password, salt)
-// })
+userSchema.pre('save', async function (next) {
+     if (!this.isModified('password')) {
+         next()
+     }
+     const salt = bcrypt.genSaltSync(10)
+     this.password = await bcrypt.hash(this.password, salt)
+})
 // userSchema.methods = {
 //     isCorrectPassword: async function (password) {
 //         return await bcrypt.compare(password, this.password)
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,14 @@
+
 const userRouter = require('../routes/user')
+const { notFound, errHandler } = require('../middlewares/errHandler')
 
 const initRoutes = (app) => {
     app.use('/api/user', userRouter)
+
+
+
+    app.use(notFound)
+    app.use(errHandler)
 }
 
 module.exports = initRoutes
