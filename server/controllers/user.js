9aa884ec033990014ee25f7d6c95c3cc591a9efd
tvhtranhuy <tvhtranhuy1408@gmail.com>
2024-10-03 12:23:59 +0700

    login

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,8 @@
 
+const user = require('../models/user')
 const User = require('../models/user')
 const asyncHandler = require('express-async-handler')
+
 const register = asyncHandler(async(req, res)=>{
     const {email, password, firstname, lastname} = req.body
     if (!email||!password||!firstname||!lastname)
@@ -8,13 +10,40 @@ const register = asyncHandler(async(req, res)=>{
             sucess: false,
             mes:'Missing inputs'
         })
-    const response = await User.create(req.body)
-    return res.status(200).json({
-        sucess: response ? true : false,
-        response
-    })
-})
 
+    const user = await User.findOne({email})
+    if (user) throw new Error('User has existed')
+    else {
+        const newUser = await User.create(req.body)
+        return res.status(200).json({
+            sucess: newUser ? true : false,
+            mes: newUser ? 'Register is successfully. please go login': 'something went wrong'
+
+        })
+    }
+})
+const login = asyncHandler(async(req, res)=>{
+    const {email, password} = req.body
+    if (!email||!password)
+        return res.status(400).json({
+            sucess: false,
+            mes:'Missing inputs'
+        })
+// plain object
+    const response = await User.findOne({email})
+
+    if (response && await response.isCorrectPassword(password)){
+        const {password, role, ...userData} = response.toObject()
+        return res.status(200).json({
+            sucess: true,
+            userData
+        })
+    }else{
+        throw new Error('invarlid credentials')
+    }
+
+})
 module.exports = {
-    register
+    register,
+    login
 }
--- server/models/user.js
+++ server/models/user.js
@@ -68,17 +68,18 @@ userSchema.pre('save', async function (next) {
      const salt = bcrypt.genSaltSync(10)
      this.password = await bcrypt.hash(this.password, salt)
 })
-// userSchema.methods = {
-//     isCorrectPassword: async function (password) {
-//         return await bcrypt.compare(password, this.password)
-//     },
+
+userSchema.methods = {
+    isCorrectPassword: async function (password) {
+        return await bcrypt.compare(password, this.password)
+    }
 //     createPasswordChangedToken: function () {
 //         const resetToken = crypto.randomBytes(32).toString('hex')
 //         this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex')
 //         this.passwordResetExpires = Date.now() + 15 * 60 * 1000
 //         return resetToken
 //     }
-// }
+}
 
 //Export the model
 module.exports = mongoose.model('User', userSchema);
