54849164acd67874252e5857bc298a3e5a01e855
tvhtranhuy <tvhtranhuy1408@gmail.com>
2024-10-03 18:46:30 +0700

    logout

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 const ctrls = require('../controllers/user')
 const {verifyAccessToken} = require('../middlewares/verifyToken')
 
-router.post("/register", ctrls.register)
-router.post("/login", ctrls.login)
-router.get('/current',verifyAccessToken ,ctrls.getCurrent)
+router.post('/register', ctrls.register)
+router.post('/login', ctrls.login)
+router.get('/current',verifyAccessToken, ctrls.getCurrent)
+router.post('/refreshtoken', ctrls.refreshAccessToken)
+router.get('/logout', ctrls.logout)
 
 module.exports = router
 
