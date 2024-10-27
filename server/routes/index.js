f3708b879fd367efd5808a923efa3db12f23d84b
tvhtranhuy <tvhtranhuy1408@gmail.com>
2024-10-27 18:15:30 +0700

    Blog

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
 const userRouter = require('./user')
 const productRouter = require('./product')
 const productCategoryRouter = require('./productCategory')
+const blogCategoryRouter = require('./blogCategory')
 const { notFound , errHandler } = require('../middlewares/errHandler')
 
 const initRoutes = (app) => {
     app.use('/api/user', userRouter)
     app.use('/api/product', productRouter)
+    app.use('/api/blogcategory', blogCategoryRouter)
     app.use('/api/prodcategory', productCategoryRouter)
 
 
