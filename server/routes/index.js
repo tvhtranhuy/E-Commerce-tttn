a2cdffe7822237dbcd50a48e9b851e11da0b81ee
tvhtranhuy <tvhtranhuy1408@gmail.com>
2024-10-27 18:02:31 +0700

    Category

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 const userRouter = require('./user')
 const productRouter = require('./product')
+const productCategoryRouter = require('./productCategory')
 const { notFound , errHandler } = require('../middlewares/errHandler')
 
 const initRoutes = (app) => {
     app.use('/api/user', userRouter)
     app.use('/api/product', productRouter)
+    app.use('/api/prodcategory', productCategoryRouter)
 
 
 
